4d68e05fcf19cad255eccda649a894a900315c3c
Josebautista10 <cooldj1020@gmail.com>
2022-06-19 01:30:11 -0600

    add animations on components

--- src/components/Certificates/Certificate.tsx
+++ src/components/Certificates/Certificate.tsx
@@ -8,35 +8,34 @@ export default function Certificate() {
         <div className="flex flex-col w-full mb-20">
           <AcademicCapIcon className="mx-auto inline-block w-10 mb-4" />
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
-            Apps I've Built
+            My Certificates
           </h1>
           {/* <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
             
           </p> */}
         </div>
         <div className="flex flex-wrap -m-4">
-          {certificates.map((project) => (
-            <a
-              href={project.link}
-              key={project.image}
+          {certificates.map((certificate) => (
+            <div
+              // key={certificate.image}
               className="sm:w-1/2 w-100 p-4">
               <div className="flex relative">
                 <img
-                  alt={project.title}
+                  // alt={certificate.title}
                   className="absolute inset-0 w-full h-full object-cover object-center"
-                  src={project.image}
+                  // src={certificate.image}
                 />
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
-                    {project.stack}
+                    {/* {certificate.stack} */}
                   </h2>
                   <h1 className="title-font text-lg font-medium text-white mb-3">
-                    {project.title}
+                    {/* {certificate.title} */}
                   </h1>
-                  <p className="leading-relaxed">{project.description}</p>
+                  {/* <p className="leading-relaxed">{certificate.description}</p> */}
                 </div>
               </div>
-            </a>
+            </div>
           ))}
         </div>
       </div>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import TypeAnimation from 'react-type-animation'
 
 const Home = () => {
   return (
@@ -8,8 +8,13 @@ const Home = () => {
           <div className='lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center'>
             <h1 className='title-font sm:text-4xl text-3xl mb-4 font-medium text-white'>
               Hi, I'm Jose Bautista.
-              <br className=' lg:inline-block' />I have a passion for building
-              cool apps.
+              <br className=' lg:inline-block' />
+              <TypeAnimation
+                cursor={true}
+                sequence={['Junior Software Developer', 1000, 'Passion for Front-end', 1000, 'Young and willing to learn', 1000, '']}
+                wrapper='h2'
+                repeat={Infinity}
+              />
             </h1>
             <p className='mb-8 leading-relaxed'>
               I'm a full stack junior developer with a passion for front-end. My
