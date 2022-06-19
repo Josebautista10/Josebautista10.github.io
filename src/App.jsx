be6c1d931fa2feb4285618aa0865d4ca6ed2e18c
Josebautista10 <cooldj1020@gmail.com>
2022-06-18 23:08:05 -0600

    build project component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Home from './components/Home'
+import Projects from './components/Projects/Project'
 import './tailwind.css'
 
 function App() {
   return (
     <div className='text-gray-400 bg-gray-900 body-font'>
       <Home />
+      <Projects/>
     </div>
   )
 }
