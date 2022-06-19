39eb797f23d00478094d2266aeaeec537d62c7e9
Josebautista10 <cooldj1020@gmail.com>
2022-06-19 00:29:41 -0600

    add all projects

--- src/components/Projects/data.tsx
+++ src/components/Projects/data.tsx
@@ -1,3 +1,34 @@
 export const projects = [
-  
+  {
+    title: "Workland",
+    stack: "React, Express, NES.CSS, Node, Postgres",
+    description:
+      "WorkLand is an 8bit metaverse for tech teams. Team members can gather in WorkLand, manage projects, track tasks and get work done.",
+    image: "./workland.png",
+    link: "https://github.com/Josebautista10/WorkLand",
+  },
+  {
+    title: "My Weather",
+    stack: "React, TypeScript, TailwindCSS, NextJS",
+    description:
+      "A weather app that uses an API to fetch the current temperature of the city you input and also a three day forecast",
+    image: "./project-2.gif",
+    link: "https://reedbarger.com",
+  },
+  {
+    title: "Categorizer",
+    stack: "JavaScript, EJS, CSS, Postgres",
+    description:
+      "An app where a user inputs an item and then it gets categorized into a specific category using different API's",
+    image: "./categorizer.png",
+    link: "https://github.com/Josebautista10/categorizer",
+  },
+  {
+    title: "Interview Scheduler",
+    stack: "React, SASS, postgres",
+    description:
+      "This App let's you see the appointments that are available for that certain day and let's you create, edit and delete an appointment.",
+    image: "./new-appt.png",
+    link: "https://github.com/Josebautista10/interview-scheduler",
+  },
 ];
