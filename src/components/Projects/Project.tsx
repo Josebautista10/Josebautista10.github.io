fae4242f3ed7bb112bc93e9dc45209be710ef456
Josebautista10 <cooldj1020@gmail.com>
2022-07-04 13:02:46 -0400

    added target blank on the links

--- src/components/Projects/Project.tsx
+++ src/components/Projects/Project.tsx
@@ -25,6 +25,8 @@ export default function Project() {
               href={project.link}
               key={project.image}
               className="sm:w-1/2 w-100 p-4"
+              target="_blank"
+              rel="noreferrer"
             >
               <div className="flex relative">
                 <img
